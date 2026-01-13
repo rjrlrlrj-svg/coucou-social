aba3acca6808bc3ad2472a8b800e89891dc65605
CouCou Builder <builder@coucou.local>
2026-01-13 19:04:09 +0800

    feat: 集成 Capacitor，支持打包为 Android App

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.coucou.social',
+  appName: 'CouCou',
+  webDir: 'dist'
+};
+
+export default config;
